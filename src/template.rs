258c22454a7b3d45c1087ffe04b0479436424081
jonpas <jonpas33@gmail.com>
2019-03-28 19:52:57 +0100

    Add Git Template Helper

--- src/template.rs
+++ src/template.rs
@@ -7,5 +7,6 @@ use crate::error::*;
 pub fn render(source: &String, data: &BTreeMap<&'static str, String>) -> String {
     let mut handlebars = Handlebars::new();
     handlebars.register_helper("date", Box::new(crate::helpers::date));
+    handlebars.register_helper("git", Box::new(crate::helpers::git));
     handlebars.render_template(&source, &data).unwrap_or_print()
 }
