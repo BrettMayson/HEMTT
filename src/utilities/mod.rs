71379549b8d2eecc16af211647ca06c14075eee0
Brett <brett@bmandesigns.com>
2019-04-04 09:03:35 -0600

    SemVer templating (#92)

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -7,6 +7,7 @@ pub mod zip;
 #[derive(Debug, Deserialize)]
 pub enum Utility {
     ConvertProject,
+    Template,
     Translation,
     Zip,
 }
@@ -14,16 +15,23 @@ pub enum Utility {
 pub fn find(utility: &str) -> Option<Utility> {
     return match utility {
         "convertproject" => Some(Utility::ConvertProject),
+        "template" => Some(Utility::Template),
         "translation" => Some(Utility::Translation),
         "zip" => Some(Utility::Zip),
         _ => None
     }
 }
 
-pub fn run(utility: &Utility, args: &Vec<String>) -> Result<(), std::io::Error> {
+pub fn run(utility: &Utility, args: &mut Vec<String>) -> Result<(), std::io::Error> {
     #[allow(unreachable_patterns)]
     return match utility {
         Utility::ConvertProject => convert::run(),
+        Utility::Template => {
+            let p = crate::project::get_project()?;
+            args.remove(0);
+            println!("{}", p.render(&args.join(" ").to_owned()));
+            Ok(())
+        },
         Utility::Translation => translation::check(),
         Utility::Zip => zip::archive(args),
         _ => Err(error!("Utility not implemented"))
