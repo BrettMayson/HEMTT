e1863d5a70ddb16398c4e177a5b8007b087f0ae0
Brett <brett@bmandesigns.com>
2019-06-01 04:21:26 -0600

    convert files using serde-arma

--- src/utilities/convert.rs
+++ src/utilities/convert.rs
@@ -1,22 +1,55 @@
-use std::fs;
+use docopt::Docopt;
+use serde::Deserialize;
+use serde_arma;
+use serde_json;
+use serde_transcode;
+
 use std::fs::File;
+use std::io::{Write, BufReader, BufWriter};
+use std::path::PathBuf;
 
-use crate::error::*;
+const USAGE: &'static str = "
+Convert Arma 3 Configs to other formats for use in external programs.
 
-pub fn run() -> Result<(), std::io::Error> {
-    crate::check(false, false)?;
-    let p = crate::project::get_project()?;
-    let file = crate::project::path(false).unwrap_or_print();
-    match file.extension().unwrap().to_str().unwrap() {
-        "toml" => {
-            fs::remove_file("hemtt.toml")?;
-            File::create("hemtt.json")?;
-        },
-        "json" => {
-            fs::remove_file("hemtt.json")?;
-            File::create("hemtt.toml")?;
-        },
-        _ => unreachable!()
+Usage:
+  convert <input> <output>
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_input: String,
+    arg_output: OutputFormat,
+}
+
+// Ability to support additonal formats in the future
+#[derive(Debug, Deserialize)]
+enum OutputFormat {
+    JSON,
+}
+
+impl std::fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
-    p.save()
+}
+
+pub fn run (usage: &Vec<String>) -> Result<(), std::io::Error> {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.argv(usage.into_iter()).deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    let reader = BufReader::new(File::open(&args.arg_input).unwrap());
+
+    let mut output = PathBuf::from(args.arg_input);
+    output.set_extension(args.arg_output.to_string().to_lowercase());
+    let writer = BufWriter::new(File::create(output).unwrap());
+
+    // Only JSON is currently supported
+    let mut deserializer = serde_arma::from_reader(reader);
+    let mut serializer = serde_json::Serializer::pretty(writer);
+
+    serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
+    serializer.into_inner().flush().unwrap();
+
+    Ok(())
 }
--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 use std::str::FromStr;
 
 pub mod convert;
+pub mod convert_project;
 pub mod translation;
 pub mod zip;
 pub mod armake;
@@ -10,6 +11,7 @@ pub mod armake;
 #[derive(Debug, Deserialize)]
 pub enum Utility {
     Armake,
+    Convert,
     ConvertProject,
     Template,
     Translation,
@@ -20,6 +22,7 @@ impl FromStr for Utility {
     fn from_str(s: &str) -> Result<Utility, ()> {
         match s.to_lowercase().as_str() {
             "armake" => Ok(Utility::Armake),
+            "convert" => Ok(Utility::Convert),
             "convertproject" => Ok(Utility::ConvertProject),
             "translation" => Ok(Utility::Translation),
             "zip" => Ok(Utility::Zip),
@@ -29,13 +32,9 @@ impl FromStr for Utility {
 }
 
 pub fn find(utility: &str) -> Option<Utility> {
-    return match utility {
-        "armake" => Some(Utility::Armake),
-        "convertproject" => Some(Utility::ConvertProject),
-        "template" => Some(Utility::Template),
-        "translation" => Some(Utility::Translation),
-        "zip" => Some(Utility::Zip),
-        _ => None
+    return match Utility::from_str(utility) {
+        Ok(v) => Some(v),
+        Err(_) => None,
     }
 }
 
@@ -43,7 +42,8 @@ pub fn run(utility: &Utility, args: &mut Vec<String>) -> Result<(), std::io::Err
     #[allow(unreachable_patterns)]
     return match utility {
         Utility::Armake => armake::run(args),
-        Utility::ConvertProject => convert::run(),
+        Utility::Convert => convert::run(args),
+        Utility::ConvertProject => convert_project::run(),
         Utility::Template => {
             let p = crate::project::get_project()?;
             args.remove(0);
