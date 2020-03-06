2cf6fb74b3e7c569e746bc9bd2316179842c4e1a
Brett <brett@mayson.io>
2020-03-05 21:00:30 -0600

    get rid of travis

--- ci/version.sh
+++
@@ -1,5 +0,0 @@
-if [ -z "$TRAVIS_TAG" ]
-then
-    HASH=$(git log --pretty=format:'%h' -n 1)
-    sed -E "s/version = \"(.+)\"$/version = \"\1-$HASH\"/" Cargo.toml > Cargo.toml.versioned && mv Cargo.toml.versioned Cargo.toml
-fi
