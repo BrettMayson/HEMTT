bb97469c75d97079a59f191f9b67cdc916003edb
Brett Mayson <brett@mayson.io>
2021-01-28 19:06:05 +0000

    wip

--- hemtt-pbo/tests/read.rs
+++ hemtt-pbo/tests/read.rs
@@ -1,13 +1,11 @@
 use std::fs::File;
 
-use hemtt_pbo::{Header, ReadablePBO};
+use hemtt_pbo::{Header, ReadablePBO, WritablePBO};
 
-fn test_pbo(file: File, file_count: usize, version: &str, prefix: &str) -> ReadablePBO<File> {
+fn test_pbo(file: File, file_count: usize, extension_count: usize, version: &str, prefix: &str) -> ReadablePBO<File> {
     let pbo = ReadablePBO::from(file).unwrap();
-    for extension in pbo.extensions() {
-        println!("Ext: {:?}", extension);
-    }
     assert_eq!(pbo.files().len(), file_count);
+    assert_eq!(pbo.extensions().len(), extension_count);
     assert_eq!(pbo.is_sorted(), true);
     assert_eq!(pbo.extension("version"), Some(&version.to_string()));
     assert_eq!(pbo.extension("prefix"), Some(&prefix.to_string()));
@@ -31,11 +29,20 @@ fn test_header(
     assert_eq!(header.size(), size);
 }
 
+fn test_file(
+    data: std::io::Cursor<Box<[u8]>>,
+    content: String,
+) {
+    let data = String::from_utf8(data.into_inner().to_vec()).unwrap();
+    assert_eq!(data, content);
+}
+
 #[test]
 fn ace_weather() {
-    let pbo = test_pbo(
+    let mut pbo = test_pbo(
         File::open("tests/ace_weather.pbo").unwrap(),
         41,
+        3,
         "cba6f72c",
         "z\\ace\\addons\\weather",
     );
@@ -48,4 +55,17 @@ fn ace_weather() {
         1543422611,
         20,
     );
+    test_header(
+        &pbo.header("$PBOPREFIX$.backup").unwrap(),
+        "$PBOPREFIX$.backup",
+        0,
+        20,
+        0,
+        1543422611,
+        20,
+    );
+    test_file(pbo.retrieve("XEH_preStart.sqf").unwrap(), "#include \"script_component.hpp\"\r\n\r\n#include \"XEH_PREP.hpp\"\r\n".to_string());
+    assert_eq!(pbo.retrieve("not_real").is_none(), true);
+    assert_eq!(pbo.header("not_real").is_none(), true);
+    let _writeable: WritablePBO<std::io::Cursor<Box<[u8]>>> = pbo.into();
 }
