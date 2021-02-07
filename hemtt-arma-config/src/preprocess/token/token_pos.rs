61eb5e582da7d04da57ce632785f86805340c375
Brett <brett@bmandesigns.com>
2021-02-07 03:52:20 -0600

    add file

---
+++ hemtt-arma-config/src/preprocess/token/token_pos.rs
@@ -0,0 +1,59 @@
+use pest::iterators::Pair;
+
+use super::{Rule, Token};
+
+#[derive(Clone, Debug)]
+pub struct TokenPos {
+    start: (usize, (usize, usize)),
+    end: (usize, (usize, usize)),
+    path: String,
+    token: Token,
+}
+
+impl TokenPos {
+    pub fn new<S: Into<String>>(path: S, pair: Pair<'_, Rule>) -> Self {
+        Self {
+            start: (
+                pair.as_span().start_pos().pos(),
+                pair.as_span().start_pos().line_col(),
+            ),
+            end: (
+                pair.as_span().end_pos().pos(),
+                pair.as_span().end_pos().line_col(),
+            ),
+            path: path.into(),
+            token: Token::from(pair),
+        }
+    }
+
+    pub fn anon(token: Token) -> Self {
+        Self {
+            start: (0, (0, 0)),
+            end: (0, (0, 0)),
+            path: String::new(),
+            token,
+        }
+    }
+
+    pub fn start(&self) -> (usize, (usize, usize)) {
+        self.start
+    }
+
+    pub fn end(&self) -> (usize, (usize, usize)) {
+        self.end
+    }
+
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    pub fn token(&self) -> &Token {
+        &self.token
+    }
+}
+
+impl ToString for TokenPos {
+    fn to_string(&self) -> String {
+        self.token().to_string()
+    }
+}
