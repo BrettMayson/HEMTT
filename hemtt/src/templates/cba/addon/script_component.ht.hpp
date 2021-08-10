9d5cd8ad45489f0f54ebaa81347742f305dd189b
Brett <brett@mayson.io>
2021-08-09 22:03:15 -0600

    cargo update, cba template fixes

--- hemtt/src/templates/cba/addon/script_component.ht.hpp
+++ hemtt/src/templates/cba/addon/script_component.ht.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT %%addon%%
-#include "\{{mainprefix}}\{{prefix}}\addons\main\script_mod.hpp"
+#include "\{{project.mainprefix}}\{{project.prefix}}\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
@@ -11,4 +11,4 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_%%ADDON%%
 #endif
 
-#include "\{{mainprefix}}\{{prefix}}\addons\main\script_macros.hpp"
+#include "\{{project.mainprefix}}\{{project.prefix}}\addons\main\script_macros.hpp"
