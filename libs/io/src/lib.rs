d8b54172f00682af486f8e60dc27deb6c8e5fe36
BrettMayson <brett@mayson.io>
2023-01-25 22:58:38 -0600

    License as GPLv2

--- libs/io/src/lib.rs
+++ libs/io/src/lib.rs
@@ -2,6 +2,13 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::cast_possible_truncation)]
 
+// Parts of the following code is derivative work of the code from the armake2 project by KoffeinFlummi, 
+// which is licensed GPLv2. This code therefore is also licensed under the terms 
+// of the GNU Public License, verison 2.
+
+// The original code can be found here:
+// https://github.com/KoffeinFlummi/armake2/blob/4b736afc8c615cf49a0d1adce8f6b9a8ae31d90f/src/io.rs
+
 use std::io;
 use std::io::{Read, Write};
 
--- libs/pbo/src/lib.rs
+++ libs/pbo/src/lib.rs
@@ -4,6 +4,13 @@
 
 //! HEMTT - Arma 3 PBO Writer/Reader
 
+// Parts of the following code is derivative work of the code from the armake2 project by KoffeinFlummi, 
+// which is licensed GPLv2. This code therefore is also licensed under the terms 
+// of the GNU Public License, verison 2.
+
+// The original code can be found here:
+// https://github.com/KoffeinFlummi/armake2/blob/4b736afc8c615cf49a0d1adce8f6b9a8ae31d90f/src/pbo.rs
+
 use std::io::{Read, Write};
 
 mod error;
