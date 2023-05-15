3fcc188274e043399f79960ab2c867ef0b53f1ee
BrettMayson <brett@mayson.io>
2023-05-15 17:53:50 -0600

    prettier panic message

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -2,8 +2,29 @@
 #![warn(clippy::pedantic)]
 
 use hemtt_error::DisplayStyle;
+use tracing::error;
 
 fn main() {
+    std::panic::set_hook(Box::new(|panic| {
+        error!("{panic}");
+        eprintln!(
+            r#"
+Oh no! HEMTT has crashed!
+This is a bug in HEMTT itself, not necessarily your project.
+Even if there is a bug in your project, HEMTT should not crash, but gracefully exit with an error message.
+
+Support for HEMTT can be found on:
+GitHub (https://github.com/BrettMayson/HEMTT)
+#hemtt on the ACE 3 Discord (https://acemod.org/discord)
+
+The log from the most recent run can be found in `.hemttout/latest.log`.
+
+It is always best to the include the log and a link to your project when reporting a bug, this will help reproduce the issue.
+"#
+        );
+        std::process::exit(1);
+    }));
+
     #[cfg(windows)]
     if ansi_term::enable_ansi_support().is_err() {
         colored::control::set_override(false);
