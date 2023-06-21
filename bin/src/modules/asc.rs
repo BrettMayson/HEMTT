eb628f743d4f881bb2f7388622258bafe9f95caf
Brett Mayson <brett@joinoffstreet.com>
2023-06-21 07:39:43 -0600

    mut

--- bin/src/modules/asc.rs
+++ bin/src/modules/asc.rs
@@ -77,7 +77,7 @@ impl Module for ArmaScriptCompiler {
         if !ctx.config().asc().enabled() {
             return Ok(());
         }
-        let out_file =
+        let mut out_file =
             File::create(".hemttout/asc.log").expect("Unable to create `.hemttout/asc.log`");
         let mut config = ASCConfig::new();
         let tmp = ctx.tmp().join("asc");
