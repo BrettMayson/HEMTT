1b646cb347a9e9de11249e5a8dd846245552ddb7
BrettMayson <brett@mayson.io>
2023-11-02 04:39:16 -0600

    mark non-release as -dev

--- bin/src/lib.rs
+++ bin/src/lib.rs
@@ -27,6 +27,8 @@ pub fn cli() -> Command {
         .version({
             let mut base = env!("CARGO_PKG_VERSION").to_string();
             if option_env!("CI").is_none() {
+                base.push_str("-local");
+            } else if option_env!("RELEASE").is_none() {
                 base.push_str("-dev");
             }
             if cfg!(debug_assertions) {
