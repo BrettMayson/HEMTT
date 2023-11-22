0e505a9d83c6e5efaf3db40080e59d4902378048
Brett Mayson <brett@mayson.io>
2023-11-22 23:04:32 +0000

    add browser files

---
+++ browser/background.js
@@ -0,0 +1,21 @@
+import init, { ImageResult } from './paa.js';
+
+async function run() {
+    // Firefox linter has trouble with 'import', so import directly here
+    let app = await init();
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.contentScriptQuery == "fetch_blob") {
+            let b = new Uint8Array(request.ab);
+            let result = new ImageResult(b);
+            let arr = new Uint8Array(app.memory.buffer, result.data_ptr(), result.data_len());
+            let blob = new Blob([arr], { type: 'image/png' });
+            let reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onload = function () {
+                sendResponse(reader.result);
+            };
+            return true;
+        }
+    });
+}
+run();
