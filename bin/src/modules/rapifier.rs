ac48004d4a1875b7749941b34b02ef411946af6f
BrettMayson <brett@mayson.io>
2024-02-18 18:42:03 -0600

    remove printlns

--- bin/src/modules/rapifier.rs
+++ bin/src/modules/rapifier.rs
@@ -131,12 +131,10 @@ pub fn rapify(addon: &Addon, path: &WorkspacePath, ctx: &Context) -> Result<Repo
                 .build_data()
                 .set_required_version(version, file.to_owned(), span);
         }
-        println!("using .bin");
         path.with_extension("bin")?
     } else {
         path.to_owned()
     };
-    println!("out: {:?}", out.as_str());
     if processed.no_rapify() {
         debug!(
             "skipping rapify for {}, as instructed by preprocessor",
