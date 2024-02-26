6f557828afe64f603315865c46cea53703aac6d4
BrettMayson <brett@mayson.io>
2024-02-26 11:50:28 -0600

    str methods

--- libs/config/src/model/str.rs
+++ libs/config/src/model/str.rs
@@ -6,3 +6,17 @@ pub struct Str {
     pub(crate) value: String,
     pub(crate) span: Range<usize>,
 }
+
+impl Str {
+    #[must_use]
+    /// Get the value
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+
+    #[must_use]
+    /// Get the span
+    pub fn span(&self) -> Range<usize> {
+        self.span.clone()
+    }
+}
