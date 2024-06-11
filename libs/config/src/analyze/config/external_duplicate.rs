bcde0ce7794e74f4cf5be136f5108e62f3386c89
Brett Mayson <brett@mayson.io>
2024-06-11 00:57:10 +0000

    fix check

--- libs/config/src/analyze/config/external_duplicate.rs
+++ libs/config/src/analyze/config/external_duplicate.rs
@@ -5,26 +5,13 @@ use hemtt_workspace::reporting::{Code, Processed};
 use crate::{analyze::codes::ce8_duplicate_external::DuplicateExternal, Class, Property};
 
 pub fn error(properties: &[Property], processed: &Processed) -> Vec<Arc<dyn Code>> {
-    let mut defined = HashMap::new();
-    error_inner(properties, &mut defined);
-    defined
-        .into_iter()
-        .filter_map(|(_, classes)| {
-            if classes.len() > 1 {
-                Some(Arc::new(DuplicateExternal::new(classes, processed)) as Arc<dyn Code>)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-fn error_inner(properties: &[Property], defined: &mut HashMap<String, Vec<Class>>) {
+    let mut defined: HashMap<String, Vec<Class>> = HashMap::new();
+    let mut errors = Vec::new();
     for property in properties {
         if let Property::Class(c) = property {
             match c {
                 Class::Root { properties } | Class::Local { properties, .. } => {
-                    error_inner(properties, defined);
+                    errors.extend(error(properties, processed));
                 }
                 Class::External { name } => {
                     defined
@@ -35,4 +22,12 @@ fn error_inner(properties: &[Property], defined: &mut HashMap<String, Vec<Class>
             }
         }
     }
+    errors.extend(defined.into_iter().filter_map(|(_, classes)| {
+        if classes.len() > 1 {
+            Some(Arc::new(DuplicateExternal::new(classes, processed)) as Arc<dyn Code>)
+        } else {
+            None
+        }
+    }));
+    errors
 }
--- libs/config/tests/errors/ce8_duplicate_external/source.hpp
+++ libs/config/tests/errors/ce8_duplicate_external/source.hpp
@@ -1,5 +1,10 @@
 class CfgAmmo {
     class BulletBase;
     class BulletBase;
-    class test: BulletBase{};
+    class test: BulletBase {
+        class BulletInfo;
+    };
+    class test2: BulletBase {
+        class BulletInfo;
+    };
 };
