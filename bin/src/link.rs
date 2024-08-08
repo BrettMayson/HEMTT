160800da5a2027b045f2ee27b9d34b1d2f924b75
BrettMayson <brett@mayson.io>
2024-08-07 18:33:22 -0600

    launch: clean up dead links (#737)

--- bin/src/link.rs
+++ bin/src/link.rs
@@ -9,15 +9,57 @@ use crate::error::Error;
 /// # Errors
 /// - [`Error::Link`] if the link could not be created
 /// - [`std::io::Error`] if the link command could not be executed
+///
+/// # Panics
+/// - If a symlinks exists and points to a real location, but fails to be read
 pub fn create_link(link: &PathBuf, target: &PathBuf) -> Result<(), Error> {
     use std::process::Command;
 
-    if link.exists() {
-        warn!(
-            "link {:?} already exists, intended to point to {:?}",
-            link, target
-        );
-        return Ok(());
+    if link.is_symlink() {
+        if link.exists() {
+            if &link.read_link().expect("link exists") == target {
+                return Ok(());
+            }
+            warn!(
+                "link {:?} already exists, intended to point to {:?}",
+                link, target
+            );
+            if dialoguer::Confirm::new()
+                .with_prompt(
+                    format!(
+                        "A link pointing to `{}` already exists. Do you want to replace it with a link pointing to `{}`?",
+                        link.display(),
+                        target.display()
+                    ))
+                .interact()?
+            {
+                trace!("removing symlink {:?}", link);
+                let out = Command::new("cmd")
+                    .arg("/C")
+                    .arg("rmdir")
+                    .arg(link)
+                    .output()?;
+                if !out.status.success() {
+                    return Err(Error::Link(
+                        String::from_utf8_lossy(&out.stderr).to_string(),
+                    ));
+                }
+            } else {
+                return Ok(());
+            }
+        } else {
+            trace!("removing broken symlink {:?}", link);
+            let out = Command::new("cmd")
+                .arg("/C")
+                .arg("rmdir")
+                .arg(link)
+                .output()?;
+            if !out.status.success() {
+                return Err(Error::Link(
+                    String::from_utf8_lossy(&out.stderr).to_string(),
+                ));
+            }
+        }
     }
 
     // attempt junction
