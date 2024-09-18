6170c1d3232d5aa554422683396cc21c2839fdd5
Brett Mayson <brett@mayson.io>
2024-09-18 01:44:39 +0000

    preprocessor: warn on mismatched casing

---
+++ libs/preprocessor/src/codes/pw4_include_case.rs
@@ -0,0 +1,67 @@
+use hemtt_workspace::reporting::{Code, Diagnostic, Label, Severity, Token};
+
+#[allow(unused)]
+/// Unexpected token
+pub struct IncludeCase {
+    /// The [`Token`] that included the file
+    tokens: Vec<Token>,
+    /// The name on disk of the file that was included
+    ident: String,
+}
+
+impl Code for IncludeCase {
+    fn ident(&self) -> &'static str {
+        "PW4"
+    }
+
+    fn severity(&self) -> Severity {
+        Severity::Warning
+    }
+
+    fn token(&self) -> Option<&Token> {
+        self.tokens.first()
+    }
+
+    fn message(&self) -> String {
+        "including a file with a different case".to_string()
+    }
+
+    fn note(&self) -> Option<String> {
+        Some("this will fail builds on platforms with case-sensitive filesystems".to_string())
+    }
+
+    fn expand_diagnostic(&self, diag: Diagnostic) -> Diagnostic {
+        diag.with_label(
+            Label::primary(
+                self.tokens
+                    .first()
+                    .expect("at least one token")
+                    .position()
+                    .path()
+                    .clone(),
+                {
+                    let first = self
+                        .tokens
+                        .first()
+                        .expect("at least one token")
+                        .position()
+                        .span();
+                    let last = self
+                        .tokens
+                        .last()
+                        .expect("at least one token")
+                        .position()
+                        .span();
+                    first.start..last.end
+                },
+            )
+            .with_message(self.label_message()),
+        )
+    }
+}
+
+impl IncludeCase {
+    pub const fn new(tokens: Vec<Token>, ident: String) -> Self {
+        Self { tokens, ident }
+    }
+}
--- libs/preprocessor/tests/bootstrap.rs
+++ libs/preprocessor/tests/bootstrap.rs
@@ -74,6 +74,7 @@ bootstrap!(if_read);
 bootstrap!(if_value);
 bootstrap!(ignore_if_has_include);
 bootstrap!(ignore_quoted);
+bootstrap!(include_case);
 bootstrap!(include_empty);
 bootstrap!(include);
 bootstrap!(join_digit);
