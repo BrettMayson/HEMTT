e47561398c128fd6d4dfa53b8443ce15551f8b57
PabstMirror <pabstmirror@gmail.com>
2024-10-15 00:56:26 -0500

    sqf: Add lint for unneeded not

--- libs/sqf/tests/lints.rs
+++ libs/sqf/tests/lints.rs
@@ -84,5 +84,6 @@ analyze!(s09_banned_command);
 analyze!(s11_if_not_else);
 analyze!(s17_var_all_caps);
 analyze!(s18_in_vehicle_check);
+analyze!(s19_extra_not);
 analyze!(s20_bool_static_comparison);
 analyze!(s21_invalid_comparisons);
