919ac260f022acd9bc28d7209c95233d034d19ef
Brett Mayson <brett@mayson.io>
2024-11-09 16:10:54 +0000

    fmt

--- libs/preprocessor/src/processor/mod.rs
+++ libs/preprocessor/src/processor/mod.rs
@@ -8,10 +8,12 @@ use hemtt_workspace::{
 };
 use peekmore::{PeekMore, PeekMoreIterator};
 
-use crate::codes::{pe26_unsupported_builtin::BuiltInNotSupported, pe2_unexpected_eof::UnexpectedEOF};
 use crate::codes::pe3_expected_ident::ExpectedIdent;
 use crate::codes::pw2_invalid_config_case::InvalidConfigCase;
 use crate::codes::{pe18_eoi_ifstate::EoiIfState, pe25_exec::ExecNotSupported};
+use crate::codes::{
+    pe26_unsupported_builtin::BuiltInNotSupported, pe2_unexpected_eof::UnexpectedEOF,
+};
 use crate::defines::Defines;
 use crate::ifstate::IfStates;
 use crate::Error;
