44b1100686967c471518bd809eb106d4479cbb7c
Brett Mayson <brett@mayson.io>
2024-11-09 03:18:23 +0000

    don't require vers header

--- libs/pbo/src/error.rs
+++ libs/pbo/src/error.rs
@@ -7,9 +7,6 @@ pub enum Error {
     /// [`std::io::Error`]
     Io(#[from] std::io::Error),
 
-    #[error("Missing Vers header")]
-    /// Missing Vers header
-    NoVersHeader,
     #[error("HEMTT does not support the encountered PBO Mime type: {0}")]
     /// HEMTT does not support the encountered PBO Mime type
     UnsupportedMime(u32),
