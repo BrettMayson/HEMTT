06fe031dffd1e37920ae94a5d4c66e8a876da687
Brett Mayson <brett@mayson.io>
2024-11-09 16:04:34 +0000

    preprocessor: prevent usage of unsupported macros

--- libs/preprocessor/src/codes/pe26_unsupported_builtin.rs
+++ libs/preprocessor/src/codes/pe26_unsupported_builtin.rs
@@ -5,15 +5,15 @@ use hemtt_workspace::reporting::{Code, Token};
 use crate::Error;
 
 #[allow(unused)]
-/// __EXEC is not a supported macro
-pub struct ExecNotSupported {
-    /// The [`Token`] of the __EXEC macro
+/// Built-in macro is not supported by HEMTT
+pub struct BuiltInNotSupported {
+    /// The [`Token`] of the built-in macro
     token: Box<Token>,
 }
 
-impl Code for ExecNotSupported {
+impl Code for BuiltInNotSupported {
     fn ident(&self) -> &'static str {
-        "PE25"
+        "PE26"
     }
 
     fn token(&self) -> Option<&Token> {
@@ -21,11 +21,15 @@ impl Code for ExecNotSupported {
     }
 
     fn message(&self) -> String {
-        "__EXEC is not supported".to_string()
+        format!("built-in macro `{}` is not supported by HEMTT", self.token.symbol())
+    }
+
+    fn note(&self) -> Option<String> {
+        Some("certain built-in macros can not be rapified at build time\nHEMTT does not support them to prevent unexpected behaviour".to_string())
     }
 }
 
-impl ExecNotSupported {
+impl BuiltInNotSupported {
     #[must_use]
     pub const fn new(token: Box<Token>) -> Self {
         Self { token }
