6047965f68cb96bf72c892a7f5159b14c36c5966
Brett Mayson <brett@mayson.io>
2025-03-14 08:37:54 -0600

    comment lead test

--- libs/wss/tests/lead.rs
+++ libs/wss/tests/lead.rs
@@ -1,12 +1,13 @@
-use hemtt_wss::Wss;
-
-#[test]
-fn lead() {
-    let wss =
-        Wss::from_ogg(&mut std::fs::File::open("tests/lead.ogg").expect("Failed to open WSS file"))
-            .expect("Failed to read WSS file");
-    let wav = wss.to_wav().expect("Failed to convert WSS to WAV");
-    std::fs::write("tests/lead.wav", &wav).expect("Failed to write WAV file");
-    let ogg = wss.to_ogg().expect("Failed to convert WSS to OGG");
-    std::fs::write("tests/lead2.ogg", &ogg).expect("Failed to write OGG file");
-}
+// Copyright music, only used for local testing
+// use hemtt_wss::Wss;
+//
+// #[test]
+// fn lead() {
+//     let wss =
+//         Wss::from_ogg(&mut std::fs::File::open("tests/lead.ogg").expect("Failed to open WSS file"))
+//             .expect("Failed to read WSS file");
+//     let wav = wss.to_wav().expect("Failed to convert WSS to WAV");
+//     std::fs::write("tests/lead.wav", &wav).expect("Failed to write WAV file");
+//     let ogg = wss.to_ogg().expect("Failed to convert WSS to OGG");
+//     std::fs::write("tests/lead2.ogg", &ogg).expect("Failed to write OGG file");
+// }
