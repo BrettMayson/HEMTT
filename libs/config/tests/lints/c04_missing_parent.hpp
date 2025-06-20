69d6a3f928275f2747dfc1f7abd8e2c8e248f644
PabstMirror <pabstmirror@gmail.com>
2025-06-20 16:35:05 -0500

    config: check if parent exists in scope (#1008)
    Co-authored-by: Brett Mayson <brett@mayson.io>

--- libs/config/tests/lints/c04_missing_parent.hpp
+++ libs/config/tests/lints/c04_missing_parent.hpp
@@ -8,3 +8,26 @@ class second: first {
 class third: other {
     value = 3;
 };
+
+class CfgVehicles {    
+    class Logic;
+    class Module_F: Logic {
+        class AttributesBase {
+            class Combo;
+        };
+    };
+    class test: Module_F {
+        scope = 2;
+        displayName = "test";
+        class Attributes { // should inherit from AttributesBase
+            class Side: Combo {
+                displayName = "example";
+            };
+        };
+        class Attributes2: AttributesBase { // correct example
+            class Side: Combo {
+                displayName = "example";
+            };
+        };
+    };
+};
