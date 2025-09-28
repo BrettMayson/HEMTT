918a05b249001215b3709073b289f24fc7fa2e42
Brett Mayson <brett@mayson.io>
2025-09-28 03:48:23 +0000

    format

---
+++ libs/format/tests/config/classes.hpp
@@ -0,0 +1,18 @@
+class Basic {
+    value = 1;
+};
+class Child: Basic {
+    value = 1.02;
+};
+class External;
+class Empty {};
+class Nested {
+    class Inner {
+        innerValue = 10;
+    };
+};
+class 2Values {
+    value1 = 1;
+    value2 = 2;
+};
+class EmptyChild: Empty {};
---
+++ libs/format/tests/config/macro.hpp
@@ -0,0 +1,6 @@
+#define VALUE {1,2,3}
+#define QUOTE(x) #x
+
+class MyClass{
+    requiredAddons[] = {QUOTE(ADDON), "ace_explosives"};
+    value=VALUE;string=QUOTE(VALUE);author=ECSTRING(common,ACETeam);};
