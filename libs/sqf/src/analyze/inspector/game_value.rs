57cee71cf56c3e3963896a18fb9988e2a2a0a4da
PabstMirror <pabstmirror@gmail.com>
2025-10-06 19:23:41 -0500

    fmt

--- libs/sqf/src/analyze/inspector/game_value.rs
+++ libs/sqf/src/analyze/inspector/game_value.rs
@@ -346,7 +346,7 @@ impl GameValue {
     }
     #[must_use]
     /// Returns the common generic type of all array elements.
-    /// 
+    ///
     /// If the set is empty or contains any non-array types, returns `Anything`.
     /// If array element types differ, returns `Anything`.
     pub fn get_array_value_type(set: &IndexSet<Self>) -> Self {
