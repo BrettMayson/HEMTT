6525fbe8b3cd6c2f591518cbac91a06fcced6d0d
PabstMirror <pabstmirror@gmail.com>
2025-10-07 19:11:59 -0500

    Inspector - Test all wiki examples

--- libs/sqf/tests/inspector.rs
+++ libs/sqf/tests/inspector.rs
@@ -27,6 +27,7 @@ fn get_statements(file: &str) -> (Processed, Statements, Database) {
 #[cfg(test)]
 mod tests {
     use crate::get_statements;
+    use hemtt_sqf::analyze::inspector::Issue;
 
     #[test]
     pub fn test_0() {
@@ -52,5 +53,84 @@ mod tests {
         let (_pro, sqf, _database) = get_statements("test_iteration.sqf");
         let issues = sqf.issues();
         insta::assert_compact_debug_snapshot!((issues.len(), issues));
+    }
+    #[test]
+    #[ignore = "more of a test of the wiki than of hemtt, may break on bad edits to the wiki"]
+    pub fn test_wiki_examples() {
+        let file = "test_wiki_examples.sqf";
+        let mut all_examples = String::new();
+        let re = regex::Regex::new(r"<sqf>(.*?)<\/sqf>").expect("regex");
+        let database = hemtt_sqf::parser::database::Database::a3(false);
+        for (_name, cmd) in database.wiki().commands().iter() {
+            if cmd.groups().contains(&String::from("Broken Commands")) {
+                continue;
+            }
+            if [
+                "menuenable",                   // Syntax 3 "do not use"
+                "execeditorscript",             // "some old editor command"
+                "hcsetgroup",                   // fixed on wiki
+                "setconvoyseparation",          // fixed on wiki
+                "actionkeysnamesarray",         // fixed on wiki
+                "speed",                        // fixed on wiki
+                "commitoverlay",                // fixed on wiki
+                "flagside",                     // fixed on wiki
+                "unregistertask",               // fixed on wiki
+                "forceatpositionrtd",           // fixed on wiki
+                "clearoverlay",                 // fixed on wiki
+                "steamgamerecordingevent",      // fixed on wiki
+                "collapseobjecttree",           // fixed on wiki
+                "menuaction",                   // fixed on wiki
+                "taskchildren",                 // fixed on wiki
+                "lognetwork",                   // fixed on wiki
+                "surfacetype",                  // fixed on wiki
+                "removecuratoreditableobjects", // fixed on wiki
+                "taskparent",                   // fixed on wiki
+                "enabledirectionstabilization", // fixed on wiki
+                "setturretlimits",              // fixed on wiki
+                "taskcustomdata",               // fixed on wiki
+                "lookatpos",                    // fixed on wiki
+                "lockcamerato",                 // fixed on wiki
+                "enablevehiclecargo",           // fixed on wiki
+            ]
+            .contains(&cmd.name().to_ascii_lowercase().as_str())
+            {
+                continue;
+            }
+            for example in cmd.examples() {
+                for cap in re.captures_iter(example) {
+                    // run each in it's own scope to avoid cross-example issues
+                    all_examples.push_str("\n[] spawn {\n");
+                    all_examples.push_str(&cap[1]);
+                    all_examples.push_str("\n};");
+                }
+            }
+        }
+
+        let folder = std::path::PathBuf::from(crate::ROOT);
+        let workspace = hemtt_workspace::Workspace::builder()
+            .physical(&folder, hemtt_workspace::LayerType::Source)
+            .finish(None, false, &hemtt_common::config::PDriveOption::Disallow)
+            .expect("for test");
+        let source = workspace.join(file).expect("for test");
+        let mut output = source.create_file().expect("for test");
+        output.write_all(all_examples.as_bytes()).expect("for test");
+
+        let processed = hemtt_preprocessor::Processor::run(
+            &source,
+            &hemtt_common::config::PreprocessorOptions::default(),
+        )
+        .expect("for test");
+        let statements = hemtt_sqf::parser::run(
+            &hemtt_sqf::parser::database::Database::a3(false),
+            &processed,
+        )
+        .expect("for test");
+        let invalid_args = statements
+            .issues()
+            .iter()
+            .filter(|i| matches!(i, Issue::InvalidArgs(..))) // ignore unused/undefined...
+            .collect::<Vec<_>>();
+        source.vfs().remove_file().expect("vfs error");
+        assert!(invalid_args.is_empty(), "{invalid_args:#?}");
     }
 }
