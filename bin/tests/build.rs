69091ddc15c72a9de964e6ba2619f8a259391080
Brett Mayson <brett@mayson.io>
2025-11-03 18:56:36 +0000

    use serial_test

--- bin/tests/build.rs
+++ bin/tests/build.rs
@@ -1,18 +1,19 @@
 #![allow(clippy::unwrap_used)]
 
 use clap::Parser;
-use sealed_test::prelude::*;
 
 use hemtt::Cli;
 
-#[sealed_test]
+#[test]
+#[serial_test::serial]
 fn build_alpha() {
     std::env::set_current_dir(format!("{}/tests/alpha", env!("CARGO_MANIFEST_DIR"))).unwrap();
     hemtt::execute(&Cli::parse_from(vec!["hemtt", "dev", "--in-test"])).unwrap();
     hemtt::execute(&Cli::parse_from(vec!["hemtt", "build", "--in-test"])).unwrap();
 }
 
-#[sealed_test]
+#[test]
+#[serial_test::serial]
 fn build_bravo() {
     std::env::set_current_dir(format!("{}/tests/bravo", env!("CARGO_MANIFEST_DIR"))).unwrap();
     hemtt::execute(&Cli::parse_from(vec!["hemtt", "script", "test"])).unwrap();
--- bin/tests/new.rs
+++ bin/tests/new.rs
@@ -3,9 +3,8 @@
 use clap::Parser;
 use hemtt::Cli;
 
-use sealed_test::prelude::*;
-
-#[sealed_test]
+#[test]
+#[serial_test::serial]
 fn new() {
     hemtt::execute(&Cli::parse_from(vec!["hemtt", "new", "test", "--in-test"])).unwrap();
 }
