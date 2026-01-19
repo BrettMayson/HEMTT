844d7a8a981063ec7a75bc39855e1bd5a26c4ca1
PabstMirror <pabstmirror@gmail.com>
2026-01-18 22:52:42 -0600

    check, use codes, add doc

--- bin/src/modules/pboprefix.rs
+++ bin/src/modules/pboprefix.rs
@@ -1,4 +1,6 @@
+use hemtt_workspace::reporting::{Code, Severity};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use std::sync::Arc;
 
 use crate::modules::Module;
 
@@ -9,31 +11,67 @@ impl Module for PboPrefix {
     fn name(&self) -> &'static str {
         "pboprefix"
     }
-    fn pre_build(
-        &self,
-        ctx: &crate::context::Context,
-    ) -> Result<crate::report::Report, crate::Error> {
+    fn check(&self, ctx: &crate::context::Context) -> Result<crate::report::Report, crate::Error> {
         let addons = ctx.addons().iter().collect::<Vec<_>>();
         let project_expected = ctx.config().expected_path(); // e.g. "z\ace\"
         trace!(
             "Checking prefixes against expected project path: {}",
             project_expected
         );
-        addons.par_iter().for_each(|addon| {
-            if let Some(config) = addon.config()
-                && config.ignore_pboprefix()
-            {
-                return;
-            }
-            let addon_expected = format!("{}addons\\{}", project_expected, addon.name());
-            if !addon
-                .prefix()
-                .to_string()
-                .eq_ignore_ascii_case(&addon_expected)
-            {
-                warn!("Unexpected pboprefix for addon: `{}`", addon.name());
-            }
-        });
-        Ok(crate::report::Report::new())
+        let results = addons
+            .par_iter()
+            .map(|addon| -> Option<Arc<dyn Code>> {
+                if let Some(config) = addon.config()
+                    && config.ignore_pboprefix()
+                {
+                    return None;
+                }
+                let addon_expected = format!("{}addons\\{}", project_expected, addon.name());
+                if addon
+                    .prefix()
+                    .to_string()
+                    .eq_ignore_ascii_case(&addon_expected)
+                {
+                    None
+                } else {
+                    Some(std::sync::Arc::new(CodePboPrefixInvalid::new(
+                        addon.name().to_string(),
+                        addon_expected,
+                    )))
+                }
+            })
+            .filter_map(|x| x)
+            .collect::<Vec<_>>();
+        let mut report = crate::report::Report::new();
+        report.extend(results);
+        Ok(report)
+    }
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub struct CodePboPrefixInvalid {
+    addon: String,
+    expected: String,
+}
+
+impl Code for CodePboPrefixInvalid {
+    fn ident(&self) -> &'static str {
+        "INVALID-PBOPREFIX"
+    }
+    fn severity(&self) -> Severity {
+        Severity::Warning
+    }
+    fn message(&self) -> String {
+        format!("Unexpected pboprefix for addon: `{}`", self.addon)
+    }
+    fn help(&self) -> Option<String> {
+        Some(format!("Expected prefix: `{}`", self.expected))
+    }
+}
+
+impl CodePboPrefixInvalid {
+    #[must_use]
+    pub const fn new(addon: String, expected: String) -> Self {
+        Self { addon, expected }
     }
 }
