8426c575f7094f5235cc8c101be4270488fe3232
liamcannon <liamcannon6@gmail.com>
2026-09-07 16:37:52 -0400

    added custom prefix for string tables & test

--- libs/stringtable/tests/lints.rs
+++ libs/stringtable/tests/lints.rs
@@ -6,7 +6,10 @@ use hemtt_stringtable::{
 };
 use hemtt_workspace::{
     LayerType,
+    addons::Addon,
+    position::{LineCol, Position},
     reporting::{Codes, WorkspaceFiles},
+    WorkspacePath,
 };
 
 const ROOT: &str = "tests/lints/";
@@ -25,6 +28,39 @@ macro_rules! lint {
 lint!(l01_sorted);
 lint!(l02_usage_single_quotes);
 lint!(l03_no_newlines_in_tags);
+
+#[test]
+fn l02_usage_custom_prefix() {
+    let folder = std::path::PathBuf::from(ROOT);
+    let workspace = hemtt_workspace::Workspace::builder()
+        .physical(&folder, LayerType::Source)
+        .finish(None, false, &hemtt_common::config::PDriveOption::Disallow)
+        .unwrap();
+    let source = workspace.join("custom_prefix.xml").unwrap();
+    let stringtable = Project::read(source).unwrap();
+    let config = hemtt_common::config::ProjectConfig::from_file(
+        &folder.join("custom_prefix.toml"),
+    )
+    .unwrap();
+    let addon = Addon::test_addon();
+    addon
+        .build_data()
+        .localizations()
+        .lock()
+        .unwrap()
+        .push((
+            "str_ls_common_confirm".to_string(),
+            Position::new(
+                LineCol(0, (1, 1)),
+                LineCol(0, (1, 1)),
+                WorkspacePath::slim_file("tests/lints/custom_prefix.xml").unwrap(),
+            ),
+        ));
+
+    let mut codes = lint_one(&stringtable, Some(&config), vec![addon]);
+    codes.retain(|code| code.ident().starts_with("L-L02"));
+    assert!(codes.is_empty(), "{codes:?}");
+}
 
 fn lint(file: &str) -> String {
     let folder = std::path::PathBuf::from(ROOT);
